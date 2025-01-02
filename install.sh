aa638c50436189a02908b2403179e749eb24a62d
Benoit Moussaud <benoit@moussaud.org>
2025-01-02 14:13:28 +0100

    Update install.sh
    restore plugin installation externaly

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ cd fonts
 cd .. && rm -rf fonts
 
 
-#zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
-#zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
 
 ########################################################################################################################
